6b43e96e47a79654851bcefebc4b4ee21ec72295
Qian <pandachain731@gmail.com>
2017-01-10 21:13:13 -0800

    add sort function to search results

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import {sortBy} from 'lodash'
 
 const DEFAULT_QUERY = 'redux';
 
@@ -37,19 +38,63 @@ const SearchInput = ({value, onChange, children, onSubmit}) =>
         <button type="submit">{children}</button>
       </form>;
 
-const SearchTable = ({list, pattern}) =>
+const SearchTable = ({list, pattern, sortKey, onSort}) =>
       <div className="table">
-        {list.map(item =>
+        <div className="table-header">
+          <span style={{ width: '40%' }}>
+            <Sort
+              sortKey={'TITLE'}
+              onSort={onSort}
+            > Title
+            </Sort>
+          </span>
+          <span style={{ width: '30%' }}>
+            <Sort
+              sortKey={'AUTHOR'}
+              onSort={onSort}
+            >
+              Author
+            </Sort>
+          </span>
+          <span style={{ width: '15%' }}>
+            <Sort
+              sortKey={'COMMENTS'}
+              onSort={onSort}
+            > Comments
+            </Sort>
+          </span>
+          <span style={{ width: '15%' }}>
+            <Sort
+              sortKey={'POINTS'}
+              onSort={onSort}
+            >
+              Points
+            </Sort>
+          </span>
+        </div>
+        {SORTS[sortKey](list).map(item =>
           <div key={item.objectID} className="table-row">
-            <span><a href={item.url}>{item.title}</a></span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
+            <span style={{ width: '40%' }}><a href={item.url}>{item.title}</a></span>
+            <span style={{ width: '30%' }}>{item.author}</span>
+            <span style={{ width: '15%' }}>{item.num_comments}</span>
+            <span style={{ width: '15%' }}>{item.points}</span>
           </div>
         )
         }
       </div>;
 
+const SORTS = {
+  NONE: list => list,
+  TITLE: list => sortBy(list, 'title'),
+  AUTHOR: list => sortBy(list, 'author'),
+  COMMENTS: list => sortBy(list, 'num_comments').reverse(),
+  POINTS: list => sortBy(list, 'points').reverse()
+
+};
+
+const Sort = ({ sortKey, onSort, children }) => <Button onClick={() => onSort(sortKey)}>
+  {children}
+</Button>
 
 class App extends Component {
   constructor(props){
@@ -58,7 +103,8 @@ class App extends Component {
       results: null,
       query: DEFAULT_QUERY,
       searchKey: '',
-      isLoading: false
+      isLoading: false,
+      sortKey: 'NONE'
     };
 
     this.setTopStories = this.setTopStories.bind(this);
@@ -66,6 +112,7 @@ class App extends Component {
     this.handleInput = this.handleInput.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.needToSearch = this.needToSearch.bind(this);
+    this.onSort = this.onSort.bind(this);
 
   }
 
@@ -113,15 +160,19 @@ class App extends Component {
     event.preventDefault();
   }
 
+  onSort(sortKey) {
+    this.setState({sortKey})
+  }
+
   render() {
-    const {results, query, searchKey, isLoading} = this.state;
+    const {results, query, searchKey, isLoading, sortKey} = this.state;
     const page = (results && results[searchKey] && results[searchKey].page) || 0;
     const list = (results && results[searchKey] && results[searchKey].hits) || [];
     return (
       <div className="page">
         <div className="interactions">
           <SearchInput value={query} onChange={this.handleInput} onSubmit={this.onSearchSubmit}>Search</SearchInput>
-          <SearchTable list={list} pattern={query}/>
+          <SearchTable list={list} pattern={query} sortKey={sortKey} onSort={this.onSort}/>
         </div>
         <div className="interactions">
             <ButtonWithLoading isLoading={isLoading} onClick={() => this.fetchTopStories(searchKey, page + 1)} > More </ButtonWithLoading>
